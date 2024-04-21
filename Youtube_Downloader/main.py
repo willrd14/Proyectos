809317d587529354a3b3aef19cd0e202aeea1a8a
williams <williamsgamer02@gmail.com>
2024-04-21 01:17:54 -0400

    YoutubeDownloader

---
+++ Youtube_Downloader/main.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+from progress.bar import Bar, ChargingBar
+import os, time, random
+
+yt = YouTube(input("Ingresa el url del video que desee descargar: "))
+print("Procesando su video...")
+bar = ChargingBar("Descargando", max = 100)
+for num in range (100):
+    stream = yt.streams.get_by_itag(22)
+    stream.download()
+    bar.next()
+bar.finish()
+print("Su video se ha descargado con exito")
